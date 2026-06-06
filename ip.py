cc5c7117c9906e3365d8b883d02cd06add05a219
MAYANK2454-19 <mk2599599@gmail.com>
2026-06-06 11:58:12 +0530

    update codes with exception handelling

--- ip.py
+++ ip.py
@@ -1,5 +1,9 @@
 import socket 
 sock= socket.socket()
-target = input("Enter your target : ")
-ip = socket.gethostbyname(target)
-print("The IP address of " + target + " is : " + ip)
+try :
+    target = input("Enter the name of your target : ")
+    ip = socket.gethostbyname(target)
+    print("The IP address of " + target + " is : " + ip)
+except socket.gaierror :
+    print(f"{target} is not a valid hostname.")
+    
--- port_check.py
+++ port_check.py
@@ -1,6 +1,9 @@
 import socket 
 sock= socket.socket()
-target = input("Enter the name of your target : ")
-port = int(input("Enter the port you want to check : "))
-result = sock.connect_ex((target,port))
-print(result)
+try :
+    target = input("Enter the name of your target : ")
+    port = int(input("Enter the port you want to check : "))
+    result = sock.connect_ex((target,port))
+    print(result)
+except socket.gaierror :
+    print(f"{target} is not a valid hostname.")
